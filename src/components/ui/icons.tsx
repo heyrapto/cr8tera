34a93ec83d5101be3389146174413ab53a5c0c96
heyrapto <kalejaiyecaleb@gmail.com>
2025-10-29 17:38:28 +0100

    feat: enhance footer layout with new links and styling, update BlueButtonEllipse icon design

--- src/components/ui/icons.tsx
+++ src/components/ui/icons.tsx
@@ -2,9 +2,21 @@ import React from "react"
 
 export const Icons = {
     BlueButtonEllipse: (props: React.SVGProps<SVGSVGElement>) =>
-        <svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-            <circle cx="5" cy="5" r="2" fill="white" stroke="#366BDB" stroke-width="6" />
-        </svg>,
+        <svg width="34" height="34" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+<rect x="0.472222" y="0.472222" width="33.0556" height="33.0556" rx="16.5278" fill="url(#paint0_linear_2378_11398)"/>
+<rect x="0.472222" y="0.472222" width="33.0556" height="33.0556" rx="16.5278" stroke="url(#paint1_linear_2378_11398)" stroke-width="0.944444"/>
+<circle cx="17.0002" cy="17.0002" r="1.88889" fill="white" stroke="white" stroke-width="5.66667"/>
+<defs>
+<linearGradient id="paint0_linear_2378_11398" x1="17" y1="-1.97693e-06" x2="32.7534" y2="20.7592" gradientUnits="userSpaceOnUse">
+<stop offset="0.0432692" stop-color="#94B2FE"/>
+<stop offset="1" stop-color="#667EDB"/>
+</linearGradient>
+<linearGradient id="paint1_linear_2378_11398" x1="17" y1="0" x2="17" y2="34" gradientUnits="userSpaceOnUse">
+<stop stop-color="white"/>
+<stop offset="1" stop-color="white" stop-opacity="0.24"/>
+</linearGradient>
+</defs>
+</svg>,
 
     WhiteButtonEllipse: (props: React.SVGProps<SVGSVGElement>) =>
         <svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
--- src/layout/footer.tsx
+++ src/layout/footer.tsx
@@ -1,6 +1,90 @@
+import { Button } from "@/components/ui/button"
+import { Icons } from "@/components/ui/icons"
+import Image from "next/image"
+import Link from "next/link"
+
 const Footer = () => {
+    const footerLinks = [
+        {
+            title: "Home",
+            href: "/",
+        },
+        {
+            title: "Home",
+            href: "/",
+        },
+        {
+            title: "Home",
+            href: "/",
+        },
+        {
+            title: "Home",
+            href: "/",
+        },
+        {
+            title: "Home",
+            href: "/",
+        },
+        {
+            title: "Home",
+            href: "/",
+        },
+        {
+            title: "Home",
+            href: "/",
+        },
+        {
+            title: "Home",
+            href: "/",
+        },
+        {
+            title: "Home",
+            href: "/",
+        },
+        {
+            title: "Home",
+            href: "/",
+        },
+        {
+            title: "Home",
+            href: "/",
+        },
+    ]
   return (
-    <footer>footer</footer>
+    <footer className="flex flex-col px-[150px] relative">
+        <div className="flex justify-between">
+            <ul className="grid grid-cols-3 gap-y-3 gap-x-2">
+                {footerLinks.map((l, i) => (
+                    <li className="inline-flex items-center gap-2">
+                        <Icons.WhiteButtonEllipse width="5px" />
+                        <Link href={l.href} className="text-lg">{l.title}</Link>
+                    </li>
+                ))}
+            </ul>
+
+            <div className="flex flex-col gap-10 w-[500px]">
+                <div className="flex justify-between items-center">
+                    <h4 className="text-[2rem] font-semibold">Email</h4>
+
+                    <div className="inline-flex">
+                    <Button variant="primary" radius="md" icon={Icons.BlueButtonEllipse}>Menu</Button>
+                    </div>
+                </div>
+
+                <input type="text" placeholder="Enter Email" className="outline-none border-b border-dotted border-white" />
+            </div>
+        </div>
+
+        <h1 className="w-full text-[20vw] text-center">Cr8tera</h1>
+
+        <Image 
+        src="./images/backgrounds/footer-grad.svg"
+        alt="Footer grad"
+        width={1500}
+        height={1500}
+        className="absolute bottom-0 left-0 right-0 mx-auto -z-10"
+        />
+    </footer>
   )
 }
 
