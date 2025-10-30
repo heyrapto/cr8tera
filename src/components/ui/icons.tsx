fa3c350a862541cea7bd2f6ffa7d834a71dbd74c
heyrapto <kalejaiyecaleb@gmail.com>
2025-10-31 00:21:29 +0100

    fix: correct stroke-width attribute to strokeWidth in SmallBlueButtonEllipse SVG for consistency with React standards

--- src/components/ui/icons.tsx
+++ src/components/ui/icons.tsx
@@ -20,7 +20,7 @@ export const Icons = {
 
     SmallBlueButtonEllipse: (props: React.SVGProps<SVGSVGElement>) =>
         <svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-            <circle cx="5" cy="5" r="2" fill="white" stroke="#366BDB" stroke-width="6" />
+            <circle cx="5" cy="5" r="2" fill="white" stroke="#366BDB" strokeWidth="6" />
         </svg>,
 
     WhiteButtonEllipse: (props: React.SVGProps<SVGSVGElement>) =>
